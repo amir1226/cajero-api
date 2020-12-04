5372339cc0d8d6f879ddb911275606bc7c55dd6e
amir1226@gmail.com <amir1226@gmail.com>
2020-12-03 19:52:47 -0500

    Actualizacion con archivos en moodle

--- db/transaction_db.py
+++ db/transaction_db.py
@@ -1,19 +1,15 @@
 from datetime import datetime
 from pydantic import BaseModel
 
-
 class TransactionInDB(BaseModel):
-
     id_transaction: int = 0
     username: str
     date: datetime = datetime.now()
     value: int
     actual_balance: int
 
-
 database_transactions = []
-generator = {"id": 0}
-
+generator = {"id":0}
 
 def save_transaction(transaction_in_db: TransactionInDB):
     generator["id"] = generator["id"] + 1
